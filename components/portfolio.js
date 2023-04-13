a56c8c155e38f0edbb4e7a4be5df833689df1032
PreethamSub <91767887+PreethamSub@users.noreply.github.com>
2023-04-13 11:28:54 +0530

    Bug Fixes

--- components/portfolio.js
+++ components/portfolio.js
@@ -3,7 +3,7 @@ import Script from 'next/script'
 export default function Portfolio(props) {
     return (
         <>
-        <main className="grid grid-cols-2 justify-center">
+        <main className="grid gap-10 md:grid-cols-2 lg:gap-10">
         <section className="my-2" id="about">
           <h2 className="text-3xl font-bold">About Me</h2>
           <p className="text-lg">
