d8c6bef5efdde55b683ea72203493388016b61b3
PreethamSub <91767887+PreethamSub@users.noreply.github.com>
2023-05-09 21:24:05 +0530

    AuthorPage

--- app/(website)/author/[slug]/author.js
+++ app/(website)/author/[slug]/author.js
@@ -4,13 +4,22 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Author({ author }) {
+  const imageProps = author?.image
+  ? urlForImage(author?.image)
+  : null;
   return (
     <Container>
       <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
         About
       </h1>
-      <div className="text-center">
-        <p className="text-lg">{author.name}</p>
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          className="rounded-full h-40 w-40 object-cover"
+          src={imageProps.src}
+          alt={`Photo of ${author.name}`}
+        />
+        <h2 className="mt-4 mb-2 text-xl font-medium text-gray-900 dark:text-white">{author.name}</h2>
+        <p className="prose text-gray-500 dark:text-gray-400">{author.bio}</p>
       </div>
     </Container>
   );
